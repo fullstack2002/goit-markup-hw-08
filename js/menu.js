4dbaab25cdc29a562f860597b7c6222ac90b1307
fullstack2002 <vika.vinnikova@gmail.com>
2022-04-25 19:36:59 +0300

    hero commit

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openMenuBtn.addEventListener('click', toggleModal);
-//   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.menu.classList.toggle('is-open');
-//   }
-// })();
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
@@ -24,8 +9,6 @@
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    // console.log("Button clicked");
-    // console.log(refs.menu.classList);
     refs.menu.classList.toggle('mobile-menu_is-hidden');
   }
 })();
